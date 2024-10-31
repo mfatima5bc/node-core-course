c92dddb3d9247f9a7cc867eda9d5b8d2c8d5fac5
Maria de Fátima <mfatima5bc@gmail.com>
2024-10-31 20:42:16 -0300

    fix: git issues

--- buffers/huge-allocation.js
+++
@@ -1,9 +0,0 @@
-
-const buff = Buffer.alloc(0.5e9); // 500,000,000 500 mb 
-console.log(ConstantSourceNode.MAX_LENGTH) // total de memória que pode ser alocada
-setInterval(() => {
-  // for (let i = 0; i< buff.length; i++) {
-  //   buff[i] = 0x22;
-  // }
-  buff.fill(0x22)
-}, 50)
